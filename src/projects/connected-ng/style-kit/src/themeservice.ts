c989b8428a3cec48714bffd8c6bbc1f835bbc7a1
mihaelocvirk <ocvirkmihael@gmail.com>
2025-12-16 07:27:39 +0100

    feat: add style changes

--- src/projects/connected-ng/style-kit/src/themeservice.ts
+++ src/projects/connected-ng/style-kit/src/themeservice.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from "@angular/core";
+import { Injectable, signal, effect } from "@angular/core";
 
 @Injectable({
     providedIn: 'root'
@@ -6,11 +6,32 @@ import { Injectable, signal } from "@angular/core";
 export class ThemeService {
 
     Themes: ThemeInfo[] = [];
+    isDarkMode = signal<boolean>(false);
 
     constructor() {
         this.registerTheme({ class: '', displayName: 'Default' });
         this.registerTheme({ class: 'connected-theme', displayName: 'Connected' });
         this.selectTheme(this.Themes[0]);
+    }
+
+    applyColorSchemeEffect = effect(() => {
+        const darkMode = this.isDarkMode();
+
+        this.applyColorScheme();
+    });
+
+    applyColorScheme() {
+        const darkMode = this.isDarkMode();
+        const themeElement = document.querySelector('.connected-theme');
+        if (themeElement) {
+            if (darkMode) {
+                themeElement.classList.add('dark-mode');
+                document.documentElement.style.colorScheme = 'dark';
+            } else {
+                themeElement.classList.remove('dark-mode');
+                document.documentElement.style.colorScheme = 'light dark';
+            }
+        }
     }
 
     public query(): ThemeInfo[] {
@@ -29,6 +50,14 @@ export class ThemeService {
 
     public selectTheme(theme: ThemeInfo) {
         this.selectedTheme.set(theme);
+    }
+
+    public toggleDarkMode() {
+        this.isDarkMode.set(!this.isDarkMode());
+    }
+
+    public setDarkMode(enabled: boolean) {
+        this.isDarkMode.set(enabled);
     }
 }
 
